2e191ae2041435bb4da062d2b144da99e9eb07df
USER\panas <Nakanya.ehoh@gmail.com>
2025-02-11 17:55:23 +0700

    การเชื่อม DB ส่วนของ Admin

--- database/connect_db.php
+++ database/connect_db.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "192.168.1.136";
 $username = "swordsman3V2";
 $password = "swordsman3!!@$%$";
 $dbname = "swordsmancms";
@@ -11,9 +11,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-
-
-
-
-?>
