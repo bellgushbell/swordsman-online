6a76e9c6fe7efd85bfda3e26d5c2084d1dbbab2e
bellgushbell <gushbellpiriyapong@gmail.com>
2025-02-11 14:25:27 +0700

    casc

--- js/layout_link_page.js
+++ js/layout_link_page.js
@@ -1,41 +1,31 @@
-fetch('layout/header.html')
-    .then(response => response.text())
-    .then(data => {
-        const headerContainer = document.getElementById('header-container');
-        if (headerContainer) {
-            headerContainer.innerHTML = data;
-        } else {
-            console.error("❌ #header-container not found in DOM");
-        }
-    });
-
-// fetch('layout/menu.html')
-//     .then(response => response.text())
-//     .then(data => {
-//         const menuContainer = document.getElementById('menu-container');
-//         if (menuContainer) {
-//             menuContainer.innerHTML = data;
+// $(document).ready(function () {
+//     // โหลด header.html
+//     $("#header-container").load("layout/header.html", function (response, status, xhr) {
+//         if (status === "error") {
+//             console.error("Error loading header:", xhr.status, xhr.statusText);
 //         } else {
-//             console.error("❌ #menu-container not found in DOM");
+//             console.log("✅ Header loaded successfully.");
 //         }
 //     });
 
-// fetch('layout/footer.html')
-//     .then(response => response.text())
-//     .then(data => {
-//         const footerContainer = document.getElementById('footer-container');
-//         if (footerContainer) {
-//             footerContainer.innerHTML = data;
+//     // โหลด menu.html
+//     $("#menu-container").load("layout/menu.html", function (response, status, xhr) {
+//         if (status === "error") {
+//             console.error("Error loading menu:", xhr.status, xhr.statusText);
 //         } else {
-//             console.error("❌ #footer-container not found in DOM");
+//             console.log("Menu loaded successfully.");
 //         }
 //     });
-// document.addEventListener("DOMContentLoaded", () => {
-//     const footerContainer = document.getElementById('footer-container');
-//     if (footerContainer) {
-//         footerContainer.style.display = "none"; // แสดง footer
-//         console.log("✅ #footer-container is loaded.");
-//     } else {
-//         console.error("❌ #footer-container is not found in DOM.");
-//     }
+
+//     โหลด footer.html
+//         $("#footer-container").load("layout/footer.html", function (response, status, xhr) {
+//             if (status === "error") {
+//                 console.error("❌ Error loading footer:", xhr.status, xhr.statusText);
+//             } else {
+//                 console.log("✅ Footer loaded successfully.");
+//             }
+//         });
+
+
+
 // });
